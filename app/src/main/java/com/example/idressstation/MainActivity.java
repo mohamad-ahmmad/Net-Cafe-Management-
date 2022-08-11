816e00395a97f98e6b7f60c20c442d76c0dccb90
m7md <eng.2002.mohammad@gmail.com>
2022-08-11 16:32:57 +0300

    Project Template

---
+++ app/src/main/java/com/example/idressstation/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.idressstation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.example.idressstation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding bind;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        bind = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(bind.getRoot());
+        Log.d("ONCREAT", "HELLO");
+
+
+
+        bind.bottomNavigationView2.setOnItemSelectedListener(e -> {
+            Log.d("GENERALSCOPE", "HELLO");
+            switch(e.getItemId()){
+                case R.id.btnHome:
+                    replaceFragment(new Home());
+                    break;
+                case R.id.btnAnalysis:
+                    Log.d("INSWITCH", "HELLO");
+                    replaceFragment(new Analysis());
+                    break;
+                case R.id.btnProfits:
+                     replaceFragment(new Profits());
+                    break;
+                case R.id.btnQueue:
+                     replaceFragment(new Queue());
+                    break;
+
+            }
+
+          return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+          FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentComp, fragment, null);
+        fragmentTransaction.commit();
+    }
+}
